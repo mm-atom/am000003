8ceaa6585681c3dd82185e739d969daad43a5a5a
taoqf <tao_qiufeng@126.com>
2020-03-19 16:28:28 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
+import am0 from '@mmstudio/am000001';
 
-export default function am3(param1: string, param2: string, param3: string) {
+/**
+ * 获取存储
+ * @param mm 固定参数
+ * @param key 键
+ * @param [default_value] 默认值
+ */
+export default function get<T>(mm: am0, key: string, default_value?: T) {
+	return mm.global[key] as T || default_value;
 }
